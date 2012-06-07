1ee756956bf9cf94a779340a31ef6a23b39ef220
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2012-06-07 18:16:04 +1000

    added sql for testdata

---
+++ create-tables.sql
@@ -0,0 +1,29 @@
+create table data (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+pc VARCHAR(100),
+cpu double
+);
+
+insert into data (pc, cpu) values ('pc1', 0.1);
+insert into data (pc, cpu) values ('pc1', 0.4);
+insert into data (pc, cpu) values ('pc1', 0.2);
+insert into data (pc, cpu) values ('pc1', 0.5);
+insert into data (pc, cpu) values ('pc1', 0.3);
+insert into data (pc, cpu) values ('pc1', 0.5);
+insert into data (pc, cpu) values ('pc1', 0.8);
+insert into data (pc, cpu) values ('pc1', 0.2);
+insert into data (pc, cpu) values ('pc1', 0.9);
+insert into data (pc, cpu) values ('pc1', 0.99);
+
+
+
+insert into data (pc, cpu) values ('pc2', 0.99);
+insert into data (pc, cpu) values ('pc2', 0.9);
+insert into data (pc, cpu) values ('pc2', 0.2);
+insert into data (pc, cpu) values ('pc2', 0.8);
+insert into data (pc, cpu) values ('pc2', 0.5);
+insert into data (pc, cpu) values ('pc2', 0.3);
+insert into data (pc, cpu) values ('pc2', 0.5);
+insert into data (pc, cpu) values ('pc2', 0.2);
+insert into data (pc, cpu) values ('pc2', 0.4);
+insert into data (pc, cpu) values ('pc2', 0.1);
