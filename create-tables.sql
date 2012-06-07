8ca887a1e0dd9b18f5fbc19c6b1ccfb8fde0316c
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2012-06-07 18:31:23 +1000

    added date field to sql

--- create-tables.sql
+++ create-tables.sql
@@ -1,29 +1,31 @@
 create table data (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 pc VARCHAR(100),
+dat DATETIME,
 cpu double
 );
 
-insert into data (pc, cpu) values ('pc1', 0.1);
-insert into data (pc, cpu) values ('pc1', 0.4);
-insert into data (pc, cpu) values ('pc1', 0.2);
-insert into data (pc, cpu) values ('pc1', 0.5);
-insert into data (pc, cpu) values ('pc1', 0.3);
-insert into data (pc, cpu) values ('pc1', 0.5);
-insert into data (pc, cpu) values ('pc1', 0.8);
-insert into data (pc, cpu) values ('pc1', 0.2);
-insert into data (pc, cpu) values ('pc1', 0.9);
-insert into data (pc, cpu) values ('pc1', 0.99);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:33:30', 0.1);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:34:00', 0.4);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:35:00', 0.2);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:36:00', 0.5);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:37:00', 0.3);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:38:00', 0.5);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:39:00', 0.8);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:40:00', 0.2);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:41:00', 0.9);
+insert into data (pc, dat, cpu) values ('pc1', '2012-01-01 23:42:00', 0.99);
 
 
 
-insert into data (pc, cpu) values ('pc2', 0.99);
-insert into data (pc, cpu) values ('pc2', 0.9);
-insert into data (pc, cpu) values ('pc2', 0.2);
-insert into data (pc, cpu) values ('pc2', 0.8);
-insert into data (pc, cpu) values ('pc2', 0.5);
-insert into data (pc, cpu) values ('pc2', 0.3);
-insert into data (pc, cpu) values ('pc2', 0.5);
-insert into data (pc, cpu) values ('pc2', 0.2);
-insert into data (pc, cpu) values ('pc2', 0.4);
-insert into data (pc, cpu) values ('pc2', 0.1);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:33:30', 0.99);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:34:00', 0.48);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:35:00', 0.2);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:36:00', 0.7);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:37:00', 0.8);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:38:00', 0.5);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:39:00', 0.3);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:40:00', 0.7);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:41:00', 0.1);
+insert into data (pc, dat, cpu) values ('pc2', '2012-01-01 23:42:00', 0.4);
+
